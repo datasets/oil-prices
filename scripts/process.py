e3a592f17488c4f3d32c96e880b0fde1ac18d863
Konstantinos Iliakis <iliakiskostis@gmail.com>
2016-02-15 23:41:39 +0200

    process is ready!

--- scripts/process.py
+++ scripts/process.py
@@ -1,41 +1,59 @@
 import csv
 import urllib
 import datautil.tabular.xls as xlstab
+import os
 
-date = 'data/'
+data = 'data/'
 archive = 'archive/'
 
+
 def download():
+    granularities = [
+        ('d', 'day'),
+        ('w', 'week'),
+        ('m', 'month'),
+        ('a', 'year')
+    ]
+    source = 'https://www.eia.gov/dnav/pet/hist_xls/RBRTE'
+    for a in granularities:
+        urllib.urlretrieve(
+            source + a[0] + '.xls',
+            archive + 'brent-' + a[1] + '.xls')
+
     source = 'http://www.eia.gov/dnav/pet/hist_xls/RWTC'
-    for a in ['d', 'w', 'm', 'a']:
-        urllib.urlretrieve(source + a + '.xls', archive+'brent-'+a+'.xls')
-    '''
-    reader = xlstab.XlsReader()
-    tabdata = reader.read(open(in_path), sheet_index=0)
-    records = tabdata.data
-    header = ['Date', 'Cost per Mb', 'Cost per Genome']
-    records = records[1:]
-    for x in records:
-        x[0] = str(x[0].year)+'-'+str(x[0].month)
-        x[1] = "%.3f" % float(x[1])
-        x[2] = "%.3f" % float(x[2])
-    #print (records)
-
-    writer = csv.writer(open(out_path, 'w'), lineterminator='\n')
-    writer.writerow(header)
-    writer.writerows(records)
-'''
+    for a in granularities:
+        urllib.urlretrieve(
+            source + a[0] + '.xls',
+            archive + 'wti-' + a[1] + '.xls')
 
 
-def process_brent_crude():
-
-
-
-def process_wti():
-
+def process():
+    for dirs, subdirs, files in os.walk(archive):
+        for file in files:
+            if 'brent' in file:
+                header = ['Date', 'Brent Spot Price']
+            elif 'wti' in file:
+                header = ['Date', 'WTI Spot Price']
+            else:
+                continue
+            reader = xlstab.XlsReader()
+            tabdata = reader.read(open(os.path.join(dirs, file)),
+                                  sheet_index=1)
+            records = tabdata.data
+            records = records[3:]
+            for x in records:
+                if 'year' in file:
+                    x[0] = str(x[0].year)
+                elif 'month' in file:
+                    x[0] = str(x[0].year) + '-' + str(x[0].month).zfill(2)
+                else:
+                    pass
+            name = data + file.split('.xls')[0] + '.csv'
+            writer = csv.writer(open(name, 'w'), lineterminator='\n')
+            writer.writerow(header)
+            writer.writerows(records)
 
 
 if __name__ == '__main__':
     download()
-    process_brent_crude()
-    process_wti()
+    process()
