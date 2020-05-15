c40cb046351c9571a10bc70fc69289ebb323d39a
Sébastien Lavoie <sebastien.lavoie@datopian.com>
2020-05-15 12:13:19 -0500

    [refactor][data]: Refactor and update data
      * Remove unused statements. Reformat with Black.
      * Run script to retrieve latest data.

--- oil_prices_flow.py
+++ oil_prices_flow.py
@@ -1,95 +1,100 @@
-import datetime
-
-from dataflows import Flow, PackageWrapper, ResourceWrapper, validate, filter_rows
+from dataflows import (
+    Flow,
+    PackageWrapper,
+    validate,
+)
 from dataflows import add_metadata, dump_to_path, load, set_type, printer
 
 
 def rename_resources(package: PackageWrapper):
-    package.pkg.descriptor['resources'][0]['name'] = 'brent-daily'
-    package.pkg.descriptor['resources'][0]['path'] = 'data/brent-daily.csv'
-    package.pkg.descriptor['resources'][1]['name'] = 'brent-week'
-    package.pkg.descriptor['resources'][1]['path'] = 'data/brent-weekly.csv'
-    package.pkg.descriptor['resources'][2]['name'] = 'brent-month'
-    package.pkg.descriptor['resources'][2]['path'] = 'data/brent-monthly.csv'
-    package.pkg.descriptor['resources'][3]['name'] = 'brent-year'
-    package.pkg.descriptor['resources'][3]['path'] = 'data/brent-year.csv'
-    package.pkg.descriptor['resources'][4]['name'] = 'wti-daily'
-    package.pkg.descriptor['resources'][4]['path'] = 'data/wti-daily.csv'
-    package.pkg.descriptor['resources'][5]['name'] = 'wti-week'
-    package.pkg.descriptor['resources'][5]['path'] = 'data/wti-weekly.csv'
-    package.pkg.descriptor['resources'][6]['name'] = 'wti-month'
-    package.pkg.descriptor['resources'][6]['path'] = 'data/wti-monthly.csv'
-    package.pkg.descriptor['resources'][7]['name'] = 'wti-year'
-    package.pkg.descriptor['resources'][7]['path'] = 'data/wti-year.csv'
+    package.pkg.descriptor["resources"][0]["name"] = "brent-daily"
+    package.pkg.descriptor["resources"][0]["path"] = "data/brent-daily.csv"
+    package.pkg.descriptor["resources"][1]["name"] = "brent-week"
+    package.pkg.descriptor["resources"][1]["path"] = "data/brent-weekly.csv"
+    package.pkg.descriptor["resources"][2]["name"] = "brent-month"
+    package.pkg.descriptor["resources"][2]["path"] = "data/brent-monthly.csv"
+    package.pkg.descriptor["resources"][3]["name"] = "brent-year"
+    package.pkg.descriptor["resources"][3]["path"] = "data/brent-year.csv"
+    package.pkg.descriptor["resources"][4]["name"] = "wti-daily"
+    package.pkg.descriptor["resources"][4]["path"] = "data/wti-daily.csv"
+    package.pkg.descriptor["resources"][5]["name"] = "wti-week"
+    package.pkg.descriptor["resources"][5]["path"] = "data/wti-weekly.csv"
+    package.pkg.descriptor["resources"][6]["name"] = "wti-month"
+    package.pkg.descriptor["resources"][6]["path"] = "data/wti-monthly.csv"
+    package.pkg.descriptor["resources"][7]["name"] = "wti-year"
+    package.pkg.descriptor["resources"][7]["path"] = "data/wti-year.csv"
 
     yield package.pkg
     res_iter = iter(package)
-    for res in  res_iter:
+    for res in res_iter:
         yield res.it
     yield from package
 
 
 def filter_out_empty_rows(rows):
     for row in rows:
-        if row['Date']:
+        if row["Date"]:
             yield row
 
 
-oil_prices = Flow(
+OIL_PRICES = Flow(
     add_metadata(
         name="oil-prices",
-        title= "Brent and WTI Spot Prices",
-        descriptor="A variety of temporal granularities for Europe Brent and WTI (West Texas Intermediate) Spot Prices.",
+        title="Brent and WTI Spot Prices",
+        descriptor=(
+            "A variety of temporal granularities for Europe Brent and WTI "
+            "(West Texas Intermediate) Spot Prices."
+        ),
         sources=[
             {
                 "name": "Daily Europe Brent Spot Price",
                 "path": "https://www.eia.gov/dnav/pet/hist_xls/RBRTEd.xls",
-                "title": "Daily Europe Brent Spot Price"
+                "title": "Daily Europe Brent Spot Price",
             },
             {
                 "name": "Weekly Europe Brent Spot Price",
                 "path": "https://www.eia.gov/dnav/pet/hist_xls/RBRTEw.xls",
-                "title": "Weekly Europe Brent Spot Price"
+                "title": "Weekly Europe Brent Spot Price",
             },
             {
                 "name": "Monthly Europe Brent Spot Price",
                 "path": "https://www.eia.gov/dnav/pet/hist_xls/RBRTEm.xls",
-                "title": "Monthly Europe Brent Spot Price"
+                "title": "Monthly Europe Brent Spot Price",
             },
             {
                 "name": "Annual Europe Brent Spot Price",
                 "path": "https://www.eia.gov/dnav/pet/hist_xls/RBRTEa.xls",
-                "title": "Annual Europe Brent Spot Price"
+                "title": "Annual Europe Brent Spot Price",
             },
             {
                 "name": "Daily Cushing, OK WTI Spot Price",
                 "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCd.xls",
-                "title": "Daily Cushing, OK WTI Spot Price"
+                "title": "Daily Cushing, OK WTI Spot Price",
             },
             {
                 "name": "Weekly Cushing, OK WTI Spot Price",
                 "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCw.xls",
-                "title": "Weekly Cushing, OK WTI Spot Price"
+                "title": "Weekly Cushing, OK WTI Spot Price",
             },
             {
                 "name": "Monthly Cushing, OK WTI Spot Price",
                 "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCm.xls",
-                "title": "Monthly Cushing, OK WTI Spot Price"
+                "title": "Monthly Cushing, OK WTI Spot Price",
             },
             {
                 "name": "Annual Cushing, OK WTI Spot Price",
                 "path": "http://www.eia.gov/dnav/pet/hist_xls/RWTCa.xls",
-                "title": "Annual Cushing, OK WTI Spot Price"
-            }
+                "title": "Annual Cushing, OK WTI Spot Price",
+            },
         ],
         licenses=[
             {
                 "name": "ODC-PDDL-1.0",
                 "path": "http://opendatacommons.org/licenses/pddl/",
-                "title": "Open Data Commons Public Domain Dedication and License v1.0"
+                "title": "Open Data Commons Public Domain Dedication and License v1.0",
             }
         ],
-        keywords=["Oil","Brent","WTI","Oil Prices","eia","oil eia"],
+        keywords=["Oil", "Brent", "WTI", "Oil Prices", "eia", "oil eia"],
         views=[
             {
                 "name": "graph",
@@ -97,71 +102,71 @@ oil_prices = Flow(
                 "resourceName": "brent-day",
                 "specType": "simple",
                 "spec": {
-                "type": "line",
+                    "type": "line",
                     "group": "Date",
-                    "series": ["Brent Spot Price"]
-                }
+                    "series": ["Brent Spot Price"],
+                },
             }
-        ]
+        ],
     ),
     load(
-        load_source='https://www.eia.gov/dnav/pet/hist_xls/RBRTEd.xls',
-        format='xls',
+        load_source="https://www.eia.gov/dnav/pet/hist_xls/RBRTEd.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     load(
-        load_source='https://www.eia.gov/dnav/pet/hist_xls/RBRTEw.xls',
-        format='xls',
+        load_source="https://www.eia.gov/dnav/pet/hist_xls/RBRTEw.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     load(
-        load_source='https://www.eia.gov/dnav/pet/hist_xls/RBRTEm.xls',
-        format='xls',
+        load_source="https://www.eia.gov/dnav/pet/hist_xls/RBRTEm.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     load(
-        load_source='https://www.eia.gov/dnav/pet/hist_xls/RBRTEa.xls',
-        format='xls',
+        load_source="https://www.eia.gov/dnav/pet/hist_xls/RBRTEa.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     load(
-        load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCd.xls',
-        format='xls',
+        load_source="http://www.eia.gov/dnav/pet/hist_xls/RWTCd.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     load(
-        load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCw.xls',
-        format='xls',
+        load_source="http://www.eia.gov/dnav/pet/hist_xls/RWTCw.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     load(
-        load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCm.xls',
-        format='xls',
+        load_source="http://www.eia.gov/dnav/pet/hist_xls/RWTCm.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     load(
-        load_source='http://www.eia.gov/dnav/pet/hist_xls/RWTCa.xls',
-        format='xls',
+        load_source="http://www.eia.gov/dnav/pet/hist_xls/RWTCa.xls",
+        format="xls",
         sheet=2,
-        skip_rows=[1,2,3],
-        headers=['Date', 'Price']
+        skip_rows=[1, 2, 3],
+        headers=["Date", "Price"],
     ),
     rename_resources,
-    set_type('Date', resources=None, type='date', format='any'),
+    set_type("Date", resources=None, type="date", format="any"),
     validate(),
     printer(),
     filter_out_empty_rows,
@@ -169,5 +174,5 @@ oil_prices = Flow(
 )
 
 
-if __name__ == '__main__':
-    oil_prices.process()
+if __name__ == "__main__":
+    OIL_PRICES.process()
